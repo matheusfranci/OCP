9317cde375f19a35f44d7f14662dd82e5f3f466c
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-05-19 01:08:11 -0300

    Create HR_Schema.sql

---
+++ SQL Topic/HR_Schema.sql
@@ -0,0 +1,14 @@
+-- Ativação via script
+SYS@orcl> @$HOME\demo\schema\human_resources\hr_main.sql
+
+specify password for HR as parameter 1:
+Enter value for 1: 2022
+
+specify default tablespeace for HR as parameter 2:
+Enter value for 2: users
+
+specify temporary tablespace for HR as parameter 3:
+Enter value for 3: temp
+
+specify log path as parameter 4:
+Enter value for 4: $HOME\demo\schema\log
