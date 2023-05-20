ecbd95c69be300debee4330e359a5621abd98047
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-05-20 02:39:17 -0300

    Update Using_Conversion_Functions_and_Conditional_Expressions.sql

--- SQL Topic/Using_Conversion_Functions_and_Conditional_Expressions.sql
+++ SQL Topic/Using_Conversion_Functions_and_Conditional_Expressions.sql
@@ -22,7 +22,8 @@ SELECT e.last_name, NULLIF(j.job_id, e.job_id)
 "Old Job ID"
  FROM employees e, job_history j
  WHERE e.employee_id = j.employee_id
- ORDER BY last_name, "Old Job ID";
+ ORDER BY last_name, "Old Job ID"; --Retornará nullo em caso de não haver mudança no cargo
+ 
 COALESCE
 O exemplo a seguir retorna um porque é o primeiro
 argumento não nulo:
